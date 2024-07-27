ec0e1bfff5992c239cfc3c6b5979c87f4e45629a
halac123b <halac123b@gmail.com>
2024-07-28 03:56:56 +0700

    Update __init__.py

--- aj/__init__.py
+++ aj/__init__.py
@@ -1,3 +1,5 @@
+import platform as pyplatform
+
 __version__ = "2.2.10"
 
 # Global state (Các biến metadata cho 1 package Python)
@@ -59,8 +61,54 @@ __all__ = [
 
 
 def detect_version():
+    """Version of package"""
     return __version__
 
 
 def detect_python():
+    """Version of Python"""
     return pyplatform.python_version()
+
+
+def detect_platform():
+    """Platform đang chạy package"""
+    base_mapping = {
+        "gentoo base system": "gentoo",
+        "centos linux": "centos",
+        "mandriva linux": "mandriva",
+        "elementary os": "ubuntu",
+        "trisquel": "ubuntu",
+        "linaro": "ubuntu",
+        "linuxmint": "ubuntu",
+        "amazon": "ubuntu",
+        "redhat enterprise linux": "rhel",
+        "red hat enterprise linux server": "rhel",
+        "oracle linux server": "rhel",
+        "fedora": "rhel",
+        "olpc": "rhel",
+        "xo-system": "rhel",
+        "kali linux": "debian",
+    }
+
+    platform_mapping = {
+        "ubuntu": "debian",
+        "rhel": "centos",
+    }
+
+    # MacOS
+    if hasattr(pyplatform, "mac_ver") and pyplatform.mac_ver()[0] != "":
+        return "osx", "osx"
+
+    # Nếu không phải Linux, return luôn
+    if pyplatform.system() != "Linux":
+        res = pyplatform.system().lower()
+        return res, res
+
+    dist = ""
+    (major, minor, _) = pyplatform.python_version_tuple()
+    major = int(major)
+    minor = int(minor)
+    if (major * 10 + minor) >= 36:
+        import distro
+
+        dist = distro.id()
